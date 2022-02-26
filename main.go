c14564659ddaf8920fe00976207a7648cbb4cfc2
ghosx <ghosx@qq.com>
2022-02-26 13:06:15 +0800

    add

--- main.go
+++ main.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Index</h1>")
+	log.Println(r.RemoteAddr, r.UserAgent())
+	_, _ = fmt.Fprintf(w, "<h1>Hello Index</h1><p>ip:"+addr+"</p>")
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Health check</h1>")
+	log.Println(r.RemoteAddr, r.UserAgent())
+	_, _ = fmt.Fprintf(w, "<h1>Health check</h1><p>ip:"+addr+"</p>")
 }
+
+func init() {
+	addr = os.Getenv("POD_IP")
+	serverPort = os.Getenv("SERVER_PORT")
+}
+
+var addr string
+var serverPort string
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/health", health)
 	fmt.Println("Server starting...")
-	http.ListenAndServe(":5030", nil)
+	_ = http.ListenAndServe(fmt.Sprintf(":%s", serverPort), nil)
 }
